4b1f8abb9a2e79d786997fd47339cdd8ede72388
Mazzaroli <carlitosmazaroli@gmail.com>
2023-10-24 18:24:59 -0300

    first commit

--- src/main/java/tarea_ivan_snytko/UserInfo.java
+++ src/main/java/tarea_ivan_snytko/UserInfo.java
@@ -4,11 +4,8 @@ Create a simple system.out app.
 Print info about the user that should be passed to the main function as a parameter.
 Compile and run the program from the command line using javac and java from JDK.
  */
-public class task1 {
+public class UserInfo {
     public static void main(String[] args) {
-        // Prints a message to indicate to the user that they should input information.
-        System.out.println("Enter user information");
-
         if (args.length == 0) {
             System.out.println("No user information provided.");
             return;
