5f8844cefd309b0a8f1029b92ae894d5c6f82070
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-02 14:24:33 -0300

    Refactor comments and use setter and getter methods in GPU class
    - Improved and updated comments for better understanding in the GPU class.
    - Implemented the missing setter method for 'boostClock' that was not present before.
    - Replaced direct field access with appropriate getter and setter methods for 'memory' and 'boostClock' fields.

--- src/main/java/homework_30_10_23/components/GPU.java
+++ src/main/java/homework_30_10_23/components/GPU.java
@@ -1,16 +1,22 @@
 package homework_30_10_23.components;
 
 // GPU Class
-public class GPU extends Component{
+public class GPU extends Component {
     // Fields for the GPU class
     protected int memory;
     protected int boostClock;
 
-    // Constructor for GPU
+    // Default constructor for GPU
+    public GPU() {
+        setMemory(0);
+        setBoostClock(0);
+    }
+
+    // Constructor for GPU with specified attributes
     public GPU(String model, int memory, int boostClock) {
         super(model);
-        this.memory = memory;
-        this.boostClock = boostClock;
+        setMemory(memory);
+        setBoostClock(boostClock);
     }
 
     // Method to display GPU details
@@ -18,16 +24,27 @@ public class GPU extends Component{
     public void displayDetails() {
         System.out.println("GPU Details:");
         System.out.println("Model: " + getModel());
-        System.out.println("Video Memory: " + memory + "GB");
-        System.out.println("Boost Clock: " + boostClock + " MHz");
+        System.out.println("Video Memory: " + getMemory() + "GB");
+        System.out.println("Boost Clock: " + getBoostClock() + " MHz");
     }
 
-    // Getters and setters for Memory
-    public int getMemory() {return memory;}
-    public void setMemory(int memory) {this.memory = memory;}
+    // Getter method for Memory
+    public int getMemory() {
+        return memory;
+    }
 
-    // Getters and setters for Boost Clock
-    public int getBoostClock() {return boostClock;}
-    public void setBoostClock(int boostClock) {this.boostClock = boostClock;}
+    // Setter method for Memory
+    public void setMemory(int memory) {
+        this.memory = memory;
+    }
+
+    // Getter method for Boost Clock
+    public int getBoostClock() {
+        return boostClock;
+    }
+
+    // Setter method for Boost Clock
+    public void setBoostClock(int boostClock) {
+        this.boostClock = boostClock;
+    }
 }
-
