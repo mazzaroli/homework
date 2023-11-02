6f654039c650b3fed20a5e0f4e2ef6cc0320f1db
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-02 14:50:15 -0300

    Refactor comments in the Main class
    - Updated and corrected various comments.

--- src/main/java/homework_30_10_23/Main.java
+++ src/main/java/homework_30_10_23/Main.java
@@ -11,7 +11,6 @@ public class Main {
     public static void main(String[] args) {
         // Creating instances of CPU, GPU, RAM, Keyboard, Monitor, and Mouse
         CPU cpu = new CPU("Core i7","Intel ",2.6);
-
         GPU gpu3080 = new GPU("Nvidia RTX 3080", 12, 1710);
         GPU gpuAMD = new GPU("AMD Radeon RX 7900XT", 20, 2535);
         GPU gpu4090 = new GPU("Nvidia RTX 4090", 24, 2610);
@@ -24,24 +23,39 @@ public class Main {
         Desktop desktop = new Desktop("ATX","Dell", "XPS 9000", cpu, gpuAMD, ram, keyboard, monitor, mouse);
         Laptop laptop = new Laptop(48,"Lenovo", "ThinkPad X1", cpu, gpu3080, ram, keyboard, monitor, mouse);
         Tablet tablet = new Tablet(true,"Apple", "iPad Pro", cpu, gpu4090, ram, keyboard, monitor, mouse);
-        Desktop desktopDefault = new Desktop();
         Computer desktopPoly = new Desktop("ATX","Dell", "XPS 9000", cpu, gpuAMD, ram, keyboard, monitor, mouse);
 
-        // Printing out specific properties of Desktop, Laptop, and Tablet instances
-        System.out.println("Desktop Form Factor: " + desktop.getFormFactor());
-        System.out.println("Battery Life In Hours: " + laptop.getBatteryLifeInHours());
+        // Creating defaults instances of Desktop, Laptop, and Tablet
+        Desktop desktopDefault = new Desktop();
+        Laptop laptopDefault = new Laptop();
+        Computer tabletDefault = new Tablet();
+        CPU cpuDefault = new CPU();
 
-        System.out.println("");
-        desktopDefault.printInfo();
+        // Printing information about various devices and components
+        System.out.println();
+        desktop.printInfo();
 
-        System.out.println("");
+        System.out.println();
         tablet.printInfo();
 
-        System.out.println("");
+        System.out.println();
+        laptop.printInfo();
+
+        System.out.println();
+        desktopDefault.printInfo();
+
+        System.out.println();
+        tabletDefault.printInfo();
+
+        System.out.println();
+        laptopDefault.printInfo();
+
+        System.out.println();
         cpu.displayDetails();
 
-        System.out.println("");
-        gpu4090.displayDetails();
+        System.out.println();
+        cpuDefault.displayDetails();
+
 
         // Override methods from Computer.java
         System.out.println("\nOverride methods from Computer.java:");
@@ -54,7 +68,7 @@ public class Main {
         System.out.println("\nOverride methods from CPU.java:");
         System.out.println(cpu.toString());
         System.out.println(cpu.hashCode());
-        System.out.println(cpu.equals(desktopPoly));
+        System.out.println(cpu.equals(cpu.hashCode()));
         System.out.println(cpu.equals(cpu));
 
         // Override methods from Monitor.java
