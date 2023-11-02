aa74be0c246a6eccf24897efb0c714422b4fd7fa
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-02 11:31:23 -0300

    Add specific fields to the Computer class
    - Updated the Computer class by adding specific fields for CPU, GPU, RAM, Keyboard, Monitor, and Mouse. These fields have been inherited from the subclasses, resulting in a more structured and organized class hierarchy.

--- src/main/java/homework_30_10_23/computer/Computer.java
+++ src/main/java/homework_30_10_23/computer/Computer.java
@@ -1,21 +1,38 @@
 package homework_30_10_23.computer;
 
+import homework_30_10_23.components.*;
+
 // Computer Class
 public class Computer {
     // Fields for the Computer class
     private String brand;
     private String model;
+    private CPU cpu;
+    private GPU gpu;
+    private RAM ram;
+    private Keyboard keyboard;
+    private Monitor monitor;
+    private Mouse mouse;
 
     // Default constructor for the Computer class
     public Computer(){
-        this.brand = "Default";
-        this.model = "Default";
     }
 
     // Constructor for the Computer class
-    public Computer(String brand, String model) {
+
+
+    public Computer(String brand, String model, CPU cpu, GPU gpu, RAM ram, Keyboard keyboard, Monitor monitor, Mouse mouse) {
         this.brand = brand;
         this.model = model;
+        this.cpu = cpu;
+        this.gpu = gpu;
+        this.ram = ram;
+        this.keyboard = keyboard;
+        this.monitor = monitor;
+        this.mouse = mouse;
+    }
+
+    public Computer(String brand, String model) {
     }
 
     // Override toString method
@@ -45,17 +62,69 @@ public class Computer {
         System.out.println(brand +" "+ model);
     }
 
-    // Getter for brand
-    public String getBrand() { return brand; }
+    public String getBrand() {
+        return brand;
+    }
 
-    // Setter for brand
-    public void setBrand(String brand) { this.brand = brand; }
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
 
-    // Getter for model
-    public String getModel() { return model; }
+    public String getModel() {
+        return model;
+    }
 
-    // Setter for model
-    public void setModel(String model) { this.model = model; }
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public CPU getCpu() {
+        return cpu;
+    }
+
+    public void setCpu(CPU cpu) {
+        this.cpu = cpu;
+    }
+
+    public GPU getGpu() {
+        return gpu;
+    }
+
+    public void setGpu(GPU gpu) {
+        this.gpu = gpu;
+    }
+
+    public RAM getRam() {
+        return ram;
+    }
+
+    public void setRam(RAM ram) {
+        this.ram = ram;
+    }
+
+    public Keyboard getKeyboard() {
+        return keyboard;
+    }
+
+    public void setKeyboard(Keyboard keyboard) {
+        this.keyboard = keyboard;
+    }
+
+    public Monitor getMonitor() {
+        return monitor;
+    }
+
+    public void setMonitor(Monitor monitor) {
+        this.monitor = monitor;
+    }
+
+    public Mouse getMouse() {
+        return mouse;
+    }
+
+    public void setMouse(Mouse mouse) {
+        this.mouse = mouse;
+    }
 }
 
 
