5d87d55899f632a22c4de1907f1f762595ebac9a
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-02 14:29:26 -0300

    Update comments and utilize setter and getter methods in RAM class
    - Improved and revised comments in the RAM class to enhance code readability and understanding.
    - Utilized setter and getter methods for the 'capacity' field, enabling better encapsulation and abstraction in the codebase.

--- src/main/java/homework_30_10_23/components/RAM.java
+++ src/main/java/homework_30_10_23/components/RAM.java
@@ -4,13 +4,23 @@ package homework_30_10_23.components;
 public class RAM {
     private int capacity;
 
-    // Constructor for RAM
-    public RAM(int capacity) { this.capacity = capacity; }
+    public RAM() {
+        // Setting the capacity to 0 in the default constructor
+        setCapacity(0);
+    }
 
-    // Getter for RAM capacity
-    public int getCapacity() { return capacity; }
+    // Constructor for RAM with a specified capacity
+    public RAM(int capacity) {
+        // Setting the capacity based on the provided parameter
+        setCapacity(capacity);
+    }
 
-    // Setter for RAM capacity
-    public void setCapacity(int capacity) { this.capacity = capacity; }
+    // // Getter and Setter for the capacity of the RAM
+    public int getCapacity() {
+        return capacity;
+    }
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
 }
 
