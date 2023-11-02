198db735fcc90cb95a3d67159d245f619902030b
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-02 14:35:11 -0300

    Move fields to parent class Computer, refactor Desktop default constructor and printInfo method
    - Transferred private CPU, GPU, RAM, Keyboard, Monitor, and Mouse fields to the parent class Computer to improve code organization and abstraction.
    - Refactored the default constructor in the Desktop class, utilizing setter methods for default value assignments and ensuring proper initialization of the superclass fields.
    - Modified method access in the printInfo method in the Desktop class, accessing superclass and subclass fields via getter methods to promote encapsulation and best practices.

--- src/main/java/homework_30_10_23/devices/Desktop.java
+++ src/main/java/homework_30_10_23/devices/Desktop.java
@@ -8,76 +8,46 @@ public class Desktop extends Computer {
     // Field for the Desktop class
     private String formFactor;
 
-    // Fields for the components of Desktop
-    private CPU cpu;
-    private GPU gpu;
-    private RAM ram;
-    private Keyboard keyboard;
-    private Monitor monitor;
-    private Mouse mouse;
-
     // Default constructor for Desktop
     public Desktop() {
-        this.formFactor = "Default";
-        this.cpu = new CPU("Default","Default",0);
-        this.gpu = new GPU("Default",0,0);
-        this.ram = new RAM(8); // Default RAM size in gigabytes
-        this.keyboard = new Keyboard("Default");
-        this.monitor = new Monitor("Default");
-        this.mouse = new Mouse("Default");
+        super();
+        setFormFactor("Default");
+        this.setCpu(new CPU());
+        this.setGpu(new GPU());
+        this.setRam(new RAM());
+        this.setKeyboard(new Keyboard());
+        this.setMonitor(new Monitor());
+        this.setMouse(new Mouse());
     }
 
     // Constructor for the Desktop class
-    public Desktop(String formFactor, String brand, String model, CPU cpu, GPU gpu, RAM ram, Keyboard keyboard, Monitor monitor, Mouse mouse) {
-        super(brand, model);
-        this.formFactor = formFactor;
-        this.cpu = cpu;
-        this.gpu = gpu;
-        this.ram = ram;
-        this.keyboard = keyboard;
-        this.monitor = monitor;
-        this.mouse = mouse;
+    public Desktop(String formFactor ,String brand, String model, CPU cpu, GPU gpu, RAM ram, Keyboard keyboard, Monitor monitor, Mouse mouse) {
+        super(brand, model, cpu, gpu, ram, keyboard, monitor, mouse);
+        setFormFactor(formFactor);
+        this.setCpu(cpu);
+        this.setGpu(gpu);
+        this.setRam(ram);
+        this.setKeyboard(keyboard);
+        this.setMonitor(monitor);
+        this.setMouse(mouse);
     }
 
     // Override the printInfo method to print Desktop information
     @Override
     public void printInfo() {
+        System.out.print("Desktop: ");
         super.printInfo();
         // Additional info for Desktop
         System.out.println(" Form Factor: " + this.formFactor +
-                " CPU: " + this.cpu.getModel() +
-                ", GPU: " + this.gpu.getModel() +
-                ", RAM: " + this.ram.getCapacity() +
-                ", Keyboard: " + this.keyboard.getType() +
-                ", Monitor: " + this.monitor.getType() +
-                ", Mouse: " + this.mouse.getType());
+                " CPU: " + this.getCpu().getModel() +
+                ", GPU: " + this.getGpu().getModel() +
+                ", RAM: " + this.getRam().getCapacity() +
+                ", Keyboard: " + this.getKeyboard().getType() +
+                ", Monitor: " + this.getMonitor().getType() +
+                ", Mouse: " + this.getMouse().getType());
     }
 
     // Getters and setters for formFactor
     public String getFormFactor() {return formFactor;}
     public void setFormFactor(String formFactor) {this.formFactor = formFactor;}
-
-    // Getters and setters for CPU
-    public CPU getCpu() { return cpu; }
-    public void setCpu(CPU cpu) { this.cpu = cpu; }
-
-    // Getters and setters for GPU
-    public GPU getGpu() { return gpu; }
-    public void setGpu(GPU gpu) { this.gpu = gpu; }
-
-    // Getters and setters for RAM
-    public RAM getRam() { return ram; }
-    public void setRam(RAM ram) { this.ram = ram; }
-
-    // Getters and setters for Keyboard
-    public Keyboard getKeyboard() { return keyboard; }
-    public void setKeyboard(Keyboard keyboard) { this.keyboard = keyboard; }
-
-    // Getters and setters for Monitor
-    public Monitor getMonitor() { return monitor; }
-    public void setMonitor(Monitor monitor) { this.monitor = monitor; }
-
-    // Getters and setters for Mouse
-    public Mouse getMouse() { return mouse; }
-    public void setMouse(Mouse mouse) { this.mouse = mouse; }
 }
