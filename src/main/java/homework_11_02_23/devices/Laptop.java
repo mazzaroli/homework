3f825a0fda75a22b75a32c5ae37fbf375f7f8760
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-05 16:44:05 -0300

    Implement 'upgrade' method in Laptop class

--- src/main/java/homework_11_02_23/devices/Laptop.java
+++ src/main/java/homework_11_02_23/devices/Laptop.java
@@ -2,9 +2,10 @@ package homework_11_02_23.devices;
 
 import homework_11_02_23.components.Mouse;
 import homework_11_02_23.computer.Computer;
+import homework_11_02_23.interfaces.Upgradable;
 
 // Laptop Class
-public class Laptop extends Computer {
+public class Laptop extends Computer implements Upgradable {
     // Field for the Laptop class
     private int batteryLifeInHours;
 
@@ -21,8 +22,8 @@ public class Laptop extends Computer {
     }
 
     // Constructor for the Laptop class
-    public Laptop(int batteryLifeInHours, String brand, String model, homework_11_02_23.components.CPU cpu, homework_11_02_23.components.GPU gpu, homework_11_02_23.components.RAM ram, homework_11_02_23.components.Keyboard keyboard, homework_11_02_23.components.Monitor monitor, Mouse mouse) {
-        super(brand, model, cpu, gpu, ram, keyboard, monitor, mouse);
+    public Laptop(int batteryLifeInHours, String brand, String model, boolean isUpgradable, homework_11_02_23.components.CPU cpu, homework_11_02_23.components.GPU gpu, homework_11_02_23.components.RAM ram, homework_11_02_23.components.Keyboard keyboard, homework_11_02_23.components.Monitor monitor, Mouse mouse) {
+        super(brand, model, isUpgradable, cpu, gpu, ram, keyboard, monitor, mouse);
         setBatteryLifeInHours(batteryLifeInHours);
         this.setCpu(cpu);
         this.setGpu(gpu);
@@ -45,6 +46,17 @@ public class Laptop extends Computer {
                 ", Keyboard: " + this.getKeyboard().getType() +
                 ", Monitor: " + this.getMonitor().getType() +
                 ", Mouse: " + this.getMouse().getType());
+    }
+
+    // Check if the device is upgradable and perform the upgrade if possible; otherwise, indicate that it is not upgradable
+    @Override
+    public void upgrade() {
+        if (super.isUpgradable()) {
+            System.out.println("\nLaptop is upgradable. Performing the upgrade...");
+            // Add upgrade logic here
+        } else {
+            System.out.println("\nLaptop is not upgradable.");
+        }
     }
 
     // Getter method for BatteryLifeInHours
