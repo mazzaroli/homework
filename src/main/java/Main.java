655302fff35f02aa84c9421c2ff323683a8283fc
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-11 10:30:11 -0300

    Refactor main method in Main class

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,36 +1,45 @@
+// Importing necessary components and exceptions
 import components.*;
-import computer.Computer;
-import devices.*;
 import exceptions.*;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
 
 // Main Class
 public class Main {
-    private static final Logger LOGGER = LogManager.getLogger(Main.class);
-
-    public static void main(String[] args) throws NoGhzException, InvalidKeyboardTypeException, MonitorAlreadyOnException, MonitorAlreadyOffException, MouseNotConfigurableException {
+    public static void main(String[] args)  {
+        // Creating instances of CPU, GPU, RAM, Keyboard, Monitor, and Mouse
+        CPU cpu = null;
         try {
-            // Creating instances of CPU, GPU, RAM, Keyboard, Monitor, and Mouse
-            CPU cpu = new CPU("Core i7", "Intel", 1);
-            GPU gpu3080 = new GPU("Nvidia RTX 3080", 12, 1710);
-            GPU gpuAMD = new GPU("AMD Radeon RX 7900XT", 20, 2535);
-            GPU gpu4090 = new GPU("Nvidia RTX 4090", 24, 2610);
-            RAM ram = new RAM(16);
-            Keyboard keyboard = new Keyboard("Mechanical");
-            Monitor monitor = new Monitor("27-inch 4K", true);
-            Mouse mouse = new Mouse("Wireless", true);
+            // Creating a CPU instance with model "Core i7", manufacturer "Intel", and 1 GHz
+            cpu = new CPU("Core i7", "Intel", 1);
+        } catch (NoGhzException e) {
+            // Handling the exception if GHz value is not provided
+            throw new RuntimeException(e);
+        }
 
-            // Creating instances of Desktop, Laptop, and Tablet that are subclasses of the Computer class
-            Desktop desktop = new Desktop("ATX", "Dell", "XPS 9000", false, cpu, gpuAMD, ram, keyboard, monitor, mouse);
-            Desktop desktopDefault = new Desktop();
-            Laptop laptopDefault = new Laptop();
-            Computer tabletDefault = new Tablet();
+        Keyboard keyboard = null;
+        try {
+            // Creating a Keyboard instance with type "Mechanical"
+            keyboard = new Keyboard("Mechanical");
+        } catch (InvalidKeyboardTypeException e) {
+            // Handling the exception if an invalid keyboard type is provided
+            throw new RuntimeException(e);
+        }
 
-            monitor.setPoweredOn(true);
-            monitor.setPoweredOn(false);
-        } catch (NoGhzException | InvalidKeyboardTypeException | MouseNotConfigurableException | MonitorAlreadyOnException | MonitorAlreadyOffException e) {
-            LOGGER.error("Exception caught: " + e.getMessage());
+        Monitor monitor = null;
+        try {
+            // Creating a Monitor instance with specifications "27-inch 4K" and initially turned off
+            monitor = new Monitor("27-inch 4K", true);
+        } catch (MonitorAlreadyOffException | MonitorAlreadyOnException e) {
+            // Handling the exceptions if the monitor is already off or on
+            throw new RuntimeException(e);
+        }
+
+        Mouse mouse = null;
+        try {
+            // Creating a Mouse instance with type "Wireless" and initially configured as true
+            mouse = new Mouse("Wireless", true);
+        } catch (MouseNotConfigurableException e) {
+            // Handling the exception if the mouse configuration is not valid
+            throw new RuntimeException(e);
         }
     }
 }
