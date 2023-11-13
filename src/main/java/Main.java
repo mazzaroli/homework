aae34b5f861bd3f87e2a1003d744d4399045e159
carlosmazzaroli <carlitosmazaroli@gmail.com>
2023-11-12 21:18:28 -0300

    Refactor Main class for improved readability and enhanced inventory management

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,29 +16,38 @@ public class Main {
         ComputerInventoryManager inventoryManager = new ComputerInventoryManager();
 
         // Additional laptops for each brand
-        Laptop macBookM1 = new Laptop(10, "Apple", "MacBook M1", true,new CPU("Apple M1", "Apple", 3.2), new GPU("Integrated Apple GPU", 8, 1600),new RAM(8), new Keyboard("Membrane"), new Monitor("Retina Display", true),new Mouse("Trackpad", true));
+        Laptop macBookM1 = new Laptop(10, "Apple", "MacBook M1", true,
+                new CPU("Apple M1", "Apple", 3.2), new GPU("Integrated Apple GPU", 8, 1600),
+                new RAM(8), new Keyboard("Membrane"), new Monitor("Retina Display", true),
+                new Mouse("Trackpad", true));
 
-        Laptop macBookM2 = new Laptop(11, "Apple", "MacBook M2", true,new CPU("Apple M2", "Apple", 3.5), new GPU("Integrated Apple Pro GPU", 16, 2000),new RAM(16), new Keyboard("Membrane"), new Monitor("Pro Retina Display", true),new Mouse("Magic Trackpad", true));
+        Laptop macBookM2 = new Laptop(11, "Apple", "MacBook M40", true,
+                new CPU("Apple M2", "Apple", 3.5), new GPU("Integrated Apple Pro GPU", 16, 2000),
+                new RAM(16), new Keyboard("Membrane"), new Monitor("Pro Retina Display", true),
+                new Mouse("Magic Trackpad", true));
 
         // Addresses of branches
-        String nisseiAsuncion = "Nissei Asunción: Avenida. España 1261";
+        String nisseiAsuncion = "Nissei Asunción: Avenida España 1261";
         String ryrChaco = "RyR Computación: Santiago del Estero 414";
 
         // Add to stock
         inventoryManager.addToStock(macBookM1, nisseiAsuncion, 10);
         inventoryManager.addToStock(macBookM2, nisseiAsuncion, 20);
 
-        inventoryManager.addToStock(macBookM1, ryrChaco, 30);
-        inventoryManager.addToStock(macBookM2, ryrChaco, 40);
+        // Display inventory by branch
+        logger.info("Inventory by Branch:");
+        inventoryManager.displayInventoryByBranch();
 
         // Update stock
         inventoryManager.updateStock(macBookM1, nisseiAsuncion, 5);
+        logger.info("Stock of MacBook M1 updated in Nissei Asunción.");
 
-        // Display inventory by branch
+        // Display inventory by branch after update
+        logger.info("Updated Inventory by Branch:");
         inventoryManager.displayInventoryByBranch();
 
         // Display inventory by brand and model
-        inventoryManager.displayInventoryByBrandAndModel("Apple");
+        logger.info("Inventory by Brand and Model (Apple):");
         inventoryManager.displayInventoryByBrandAndModel("Apple");
 
         // Get stock
@@ -46,10 +55,11 @@ public class Main {
         logger.info("Stock of MacBook M1 in Nissei Asunción: " + stock);
 
         // Delete from stock
-        inventoryManager.deleteFromStock(macBookM1, nisseiAsuncion, 3);
+        inventoryManager.deleteFromStock(macBookM1, nisseiAsuncion, "All");
+        logger.info("MacBook M1 deleted from Nissei Asunción.");
 
         // Display inventory by branch after deletion
+        logger.info("Inventory by Branch after Deletion:");
         inventoryManager.displayInventoryByBranch();
-
     }
 }
