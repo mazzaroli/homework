60d5a78bcf40af62e7ba0e239a4f438992a7ada8
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-30 08:49:50 -0300

    Refactor Main Class with RAM Configurations and Device Initialization
    Summary:
    - Restructured Main class to incorporate RAM configurations and device initialization, including RAM creation using factory methods, laptop instantiation, and device information display.
    
    Details:
    - Modified Main class to incorporate functionalities related to RAM configurations and device initialization.
    - Added logic to create RAM objects using both default and custom factory methods based on specified RAM types (ComplexRAMType).
    - Implemented the creation and manipulation of a Laptop object (macBookM1) with specific configurations, including CPU, GPU, RAM, Keyboard, Monitor, and Mouse.
    - Updated Main class to demonstrate the utilization of RAM factory methods, setting RAM configurations for the laptop, and displaying device information before and after RAM modification.
    - Temporarily commented out sections related to Enum demonstrations and lambda function executions from the previous version.

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -1,11 +1,42 @@
 package com.solvd;
 
 import com.solvd.GenericFunctions.*;
-import com.solvd.enums.ComputerEnum;
-import com.solvd.functions.*;
+import com.solvd.components.*;
+import com.solvd.devices.Laptop;
+import com.solvd.enums.ComplexRAMType;
+//import com.solvd.enums.ComputerEnum;
+import com.solvd.exceptions.*;
+import com.solvd.functionalInterface.RAMFactory;
+//import com.solvd.functions.*;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoGhzException, InvalidKeyboardTypeException, MonitorAlreadyOnException, MonitorAlreadyOffException, MouseNotConfigurableException {
+        ComplexRAMType ramType = ComplexRAMType.DDR4; // Choose the RAM type you want
+        // Creating RAM using the default factory method
+        RAM ram = ramType.createRAM();
+
+        // Outputting the RAM capacity
+        System.out.println("RAM Capacity: " + ram.getCapacity());
+
+        // Creating RAM using a custom factory method (passing custom factory implementation)
+        RAMFactory customFactory = customCapacity -> new RAM(customCapacity * 2); // Custom factory that doubles the capacity
+        RAM ramCustom = ramType.createRAM(customFactory);
+
+        // Outputting the custom RAM capacity
+        System.out.println("Custom RAM Capacity: " + ramCustom.getCapacity());
+
+        Laptop macBookM1 = new Laptop(10, "Apple", "MacBook M1", true,
+                new CPU("Apple M1", "Apple", 3.2), new GPU("Integrated Apple GPU", 8, 1600),
+                ram, new Keyboard("Membrane"), new Monitor("Retina Display", true),
+                new Mouse("Trackpad", true));
+
+        macBookM1.printInfo();
+
+        macBookM1.setRam(ramCustom);
+
+        macBookM1.printInfo();
+
+        /*
         System.out.println("// Create 5 complex Enums (with fields, methods, blocks).");
         for (ComputerEnum computer : ComputerEnum.values()) {
             System.out.println("Brand: " + computer.getBrand());
@@ -39,6 +70,6 @@ public class Main {
         GenericFunctionExample.main(new String[]{});
 
         System.out.println("\nExecuting GenericPredicateExample:");
-        GenericPredicateExample.main(new String[]{});
+        GenericPredicateExample.main(new String[]{}); */
     }
 }
