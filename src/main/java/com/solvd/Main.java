651af547a711872a748205ba741bf491478b0945
Mazzaroli <carlitosmazaroli@gmail.com>
2023-11-30 10:44:00 -0300

    Enhance Main Class with Dynamic RAM Configuration Methods
    Summary:
    - Augmented the Main class to incorporate dynamic RAM configuration methods, including multiplication, division, and power operations on RAM capacity.
    
    Details:
    - Expanded the Main class to meet the task's requirement of adding two custom lambda functions with generics.
    - Implemented additional methods to dynamically configure RAM through multiplication, division, and exponentiation operations on RAM capacity, fulfilling the given task requirement.
    - Employed custom RAM factories (customFactoryMultiply, customFactoryDivide, customFactoryPower) to perform mathematical operations (doubling, halving, squaring) on RAM capacities.
    - Modified the Main class to display the current RAM capacity within a Laptop instance (macBookM1) and illustrate the effects of mathematical operations on RAM capacity.

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -1,75 +1,56 @@
+// Import necessary packages and classes
 package com.solvd;
 
-import com.solvd.GenericFunctions.*;
 import com.solvd.components.*;
 import com.solvd.devices.Laptop;
 import com.solvd.enums.ComplexRAMType;
-//import com.solvd.enums.ComputerEnum;
 import com.solvd.exceptions.*;
 import com.solvd.functionalInterface.RAMFactory;
-//import com.solvd.functions.*;
 
 public class Main {
     public static void main(String[] args) throws NoGhzException, InvalidKeyboardTypeException, MonitorAlreadyOnException, MonitorAlreadyOffException, MouseNotConfigurableException {
         ComplexRAMType ramType = ComplexRAMType.DDR4; // Choose the RAM type you want
+
         // Creating RAM using the default factory method
         RAM ram = ramType.createRAM();
 
-        // Outputting the RAM capacity
-        System.out.println("RAM Capacity: " + ram.getCapacity());
+        // Creating RAM using a custom factory method (passing custom factory implementation) for multiplication
+        RAMFactory customFactoryMultiply = customCapacity -> new RAM(customCapacity * 2); // Custom factory that doubles the capacity
+        RAM ramCustomMultiply = ramType.createRAM(customFactoryMultiply);
 
-        // Creating RAM using a custom factory method (passing custom factory implementation)
-        RAMFactory customFactory = customCapacity -> new RAM(customCapacity * 2); // Custom factory that doubles the capacity
-        RAM ramCustom = ramType.createRAM(customFactory);
+        // Creating RAM using a custom factory method for division
+        RAMFactory customFactoryDivide = customCapacity -> new RAM(customCapacity / 2); // Custom factory that divides the capacity by 2
+        RAM ramCustomDivide = ramType.createRAM(customFactoryDivide);
 
-        // Outputting the custom RAM capacity
-        System.out.println("Custom RAM Capacity: " + ramCustom.getCapacity());
+        // Creating RAM using a custom factory method for power operation
+        RAMFactory customFactoryPower = customCapacity -> new RAM((int) Math.pow(customCapacity, 2)); // Custom factory that squares the capacity
+        RAM ramCustomPower = ramType.createRAM(customFactoryPower);
 
+        // Creating a laptop instance
         Laptop macBookM1 = new Laptop(10, "Apple", "MacBook M1", true,
                 new CPU("Apple M1", "Apple", 3.2), new GPU("Integrated Apple GPU", 8, 1600),
                 ram, new Keyboard("Membrane"), new Monitor("Retina Display", true),
                 new Mouse("Trackpad", true));
 
-        macBookM1.printInfo();
+        // Print the current RAM capacity of macBookM1
+        System.out.println("Current RAM Capacity: " + macBookM1.getRam().getCapacity() + "GB");
 
-        macBookM1.setRam(ramCustom);
+        // Set the RAM with multiplication custom capacity
+        macBookM1.setRam(ramCustomMultiply);
 
-        macBookM1.printInfo();
+        // Print the RAM capacity after applying multiplication
+        System.out.println("RAM Capacity (Multiply): " + macBookM1.getRam().getCapacity() + "GB");
 
-        /*
-        System.out.println("// Create 5 complex Enums (with fields, methods, blocks).");
-        for (ComputerEnum computer : ComputerEnum.values()) {
-            System.out.println("Brand: " + computer.getBrand());
-            System.out.println("Model: " + computer.getModel());
-            System.out.println();
-        }
+        // Set the RAM with division custom capacity
+        macBookM1.setRam(ramCustomDivide);
 
-        //-----------------------------------------------------------------------------
-        System.out.println("// Use at least 5 lambda functions from the java.util.function package.");
-        System.out.println("Executing PredicateExample:");
-        PredicateExample.main(args);
+        // Print the RAM capacity after applying division
+        System.out.println("RAM Capacity (Divide): " + macBookM1.getRam().getCapacity() + "GB");
 
-        System.out.println("\nExecuting FunctionExample:");
-        FunctionExample.main(args);
+        // Set the RAM with power custom capacity
+        macBookM1.setRam(ramCustomPower);
 
-        System.out.println("\nExecuting ConsumerExample:");
-        ConsumerExample.main(args);
-
-        System.out.println("\nExecuting SupplierExample:");
-        SupplierExample.main(args);
-
-        System.out.println("\nExecuting BiFunctionExample:");
-        BiFunctionExample.main(args);
-
-        //-----------------------------------------------------------------------------
-        System.out.println("\n// Create 3 custom Lambda functions with generics.");
-        System.out.println("Executing GenericConsumerExample:");
-        GenericConsumerExample.main(new String[]{});
-
-        System.out.println("\nExecuting GenericFunctionExample:");
-        GenericFunctionExample.main(new String[]{});
-
-        System.out.println("\nExecuting GenericPredicateExample:");
-        GenericPredicateExample.main(new String[]{}); */
+        // Print the RAM capacity after applying power
+        System.out.println("RAM Capacity (Power): " + macBookM1.getRam().getCapacity() + "GB");
     }
 }
