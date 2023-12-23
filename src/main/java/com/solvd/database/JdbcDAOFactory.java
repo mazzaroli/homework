1d6c273f185e32a05872aea70d5454c8c776f7e2
Mazzaroli <carlitosmazaroli@gmail.com>
2023-12-23 00:53:22 -0300

    Subject: Add JdbcDAOFactory class
    Body:
    This commit introduces the JdbcDAOFactory class in the com.solvd.database package. This factory implementation provides JDBC-based DAO instances for CPU, GPU, and RAM entities.
    
    Key changes:
    - Created JdbcDAOFactory class responsible for generating specific JDBC-based DAO instances.
    - Includes methods to create DAO instances for CPU, GPU, and RAM entities based on the DAOType.
    - Utilizes the AbstractFactory structure for creating concrete DAO instances based on the provided DAOType.

---
+++ src/main/java/com/solvd/database/JdbcDAOFactory.java
@@ -0,0 +1,48 @@
+package com.solvd.database;
+
+import com.solvd.database.dao.IBaseDao;
+import com.solvd.database.dao.jbdc.CpuDAO;
+import com.solvd.database.dao.jbdc.GpuDAO;
+import com.solvd.database.dao.jbdc.RamDAO;
+import com.solvd.database.util.AbstractFactory;
+import com.solvd.enums.DAOType;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * Jdbc implementation of the DAO factory.
+ */
+public class JdbcDAOFactory extends AbstractFactory {
+    private static Logger logger = LogManager.getLogger(JdbcDAOFactory.class);
+
+    /**
+     * Creates a DAO instance based on the provided DAOType.
+     *
+     * @param dao The type of DAO to create (CPU, GPU, or RAM).
+     * @return An instance of IBaseDao based on the provided DAOType.
+     */
+    public static IBaseDao createDAO(DAOType dao) {
+        switch (dao) {
+            case CPU:
+                return new CpuDAO();
+            case GPU:
+                return new GpuDAO();
+            case RAM:
+                return new RamDAO();
+            default:
+                logger.error("Unexpected value: " + dao);
+                return null;
+        }
+    }
+
+    /**
+     * Gets the factory based on the provided DAOType.
+     *
+     * @param factory The type of DAO to get (CPU, GPU, or RAM).
+     * @return An instance of IBaseDao based on the provided DAOType.
+     */
+    @Override
+    public IBaseDao getFactory(DAOType factory) {
+        return createDAO(factory);
+    }
+}
