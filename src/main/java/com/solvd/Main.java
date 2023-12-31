cd089a30aea53865b96fff05aee1d8ca80f1df78
Mazzaroli <carlitosmazaroli@gmail.com>
2023-12-31 19:36:22 -0300

    Subject: Refactor Main class to support JAXB and Jackson
    Body:
    This commit updates the 'Main' class to accommodate both JAXB and Jackson functionalities for XML and JSON processing, respectively.
    
    The existing JAXB implementation for XML deserialization remains intact, ensuring compatibility with the 'Laptop' class' XML representation.
    
    Additionally, Jackson's ObjectMapper is integrated into the 'Main' class to enable JSON deserialization. The 'Main' class now includes a segment utilizing Jackson's mapper to read and deserialize a JSON file ('jacksonTest.json') into a 'Laptop' object, enhancing the application's capabilities to handle both XML and JSON data formats seamlessly.
    
    These changes allow the application to process and deserialize data from XML files using JAXB and from JSON files using Jackson, providing increased flexibility and compatibility with different data formats.

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -1,23 +1,35 @@
 package com.solvd;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.solvd.parser.models.Laptop;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
 import jakarta.xml.bind.Unmarshaller;
 
 import java.io.File;
+import java.io.IOException;
 
 
 public class Main {
     public static void main(String[] args) {
+        //JAXB
         File file = new File("src/main/resources/xml/xml.xml");
-
         try {
             JAXBContext context = JAXBContext.newInstance(Laptop.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
             Laptop laptop = (Laptop) unmarshaller.unmarshal(file);
             System.out.println(laptop);
         } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+
+        //Jackson
+        File jsonFile = new File("src/main/resources/json/jacksonTest.json");
+        ObjectMapper mapper = new ObjectMapper();
+        try {
+            Laptop laptop = mapper.readValue(jsonFile, Laptop.class);
+            System.out.println(laptop);
+        } catch (IOException e) {
             throw new RuntimeException(e);
         }
     }
