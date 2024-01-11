db929bb64c9212f40252eeb3dbbd088f325e4781
Mazzaroli <carlitosmazaroli@gmail.com>
2024-01-11 08:44:39 -0300

    Subject: Update Main class for XML writing and JSON writing
    Body:
    This commit refactors the 'Main' class to incorporate additional functionality for writing objects to both XML and JSON formats. The changes include:
    
    1. JAXB XML Writing:
       - Added code to write the deserialized 'Laptop' object to an XML file using JAXB.
       - Introduced a new XML output file ("src/main/resources/xml/output.xml").
       - Utilized the 'Marshaller' instance to enable formatted XML output.
    
    2. Jackson JSON Writing:
       - Integrated code to write the deserialized 'Laptop' object to a JSON file using Jackson.
       - Introduced a new JSON output file ("src/main/resources/json/output.json").

--- src/main/java/com/solvd/Main.java
+++ src/main/java/com/solvd/Main.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.solvd.parser.models.Laptop;
 import jakarta.xml.bind.JAXBContext;
 import jakarta.xml.bind.JAXBException;
+import jakarta.xml.bind.Marshaller;
 import jakarta.xml.bind.Unmarshaller;
 
 import java.io.File;
@@ -12,23 +13,37 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) {
-        //JAXB
-        File file = new File("src/main/resources/xml/xml.xml");
+        // JAXB - Reading from XML
+        File xmlFile = new File("src/main/resources/xml/xml.xml");
         try {
             JAXBContext context = JAXBContext.newInstance(Laptop.class);
             Unmarshaller unmarshaller = context.createUnmarshaller();
-            Laptop laptop = (Laptop) unmarshaller.unmarshal(file);
-            System.out.println(laptop);
+            Laptop laptop = (Laptop) unmarshaller.unmarshal(xmlFile);
+            System.out.println("Deserialized Object: " + laptop);
+
+            // JAXB - Writing to XML
+            File outputXmlFile = new File("src/main/resources/xml/output.xml");
+            Marshaller marshaller = context.createMarshaller();
+            marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+            marshaller.marshal(laptop, outputXmlFile);
+            System.out.println("Object successfully written to XML file.");
+
         } catch (JAXBException e) {
             throw new RuntimeException(e);
         }
 
-        //Jackson
+        // Jackson - Reading from JSON
         File jsonFile = new File("src/main/resources/json/jacksonTest.json");
         ObjectMapper mapper = new ObjectMapper();
         try {
             Laptop laptop = mapper.readValue(jsonFile, Laptop.class);
-            System.out.println(laptop);
+            System.out.println("Deserialized Object: " + laptop);
+
+            // Jackson - Writing to JSON
+            File outputJsonFile = new File("src/main/resources/json/output.json");
+            mapper.writeValue(outputJsonFile, laptop);
+            System.out.println("Object successfully written to JSON file.");
+
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
