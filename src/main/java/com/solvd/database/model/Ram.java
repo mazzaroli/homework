f49c13c6dde3c6d3cc156ab961ed745245a04540
Mazzaroli <carlitosmazaroli@gmail.com>
2024-01-17 12:37:58 -0300

    Refactor 'Ram' class with Builder Pattern and Listener Pattern
    This commit refactors the 'Ram' class by introducing the Builder Pattern to provide a more flexible and readable way of creating instances. Additionally, a private constructor and a static inner 'RamBuilder' class are added. The 'RamListener' interface is introduced for potential listeners interested in events related to 'Ram' creation.
    
    Changed fields:
    - Changed 'Computer_computer_id' to 'computer_computer_id'
    - Introduced a private constructor for internal use
    - Added a static inner 'RamBuilder' class for building instances
    - Implemented a private constructor that takes a 'RamBuilder' for building instances
    
    Tasks satisfied:
    - Builder
    - Listener

--- src/main/java/com/solvd/database/model/Ram.java
+++ src/main/java/com/solvd/database/model/Ram.java
@@ -3,30 +3,22 @@ package com.solvd.database.model;
 public class Ram {
     private Integer id;
     private Integer capacity;
-    private Integer Computer_computer_id;
+    private Integer computer_computer_id;
+
+    private Ram() {
+        // Private constructor for use by the builder
+    }
 
     public Integer getId() {
         return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
     }
 
     public Integer getCapacity() {
         return capacity;
     }
 
-    public void setCapacity(Integer capacity) {
-        this.capacity = capacity;
-    }
-
     public Integer getComputer_computer_id() {
-        return Computer_computer_id;
-    }
-
-    public void setComputer_computer_id(Integer computer_computer_id) {
-        Computer_computer_id = computer_computer_id;
+        return computer_computer_id;
     }
 
     @Override
@@ -34,7 +26,48 @@ public class Ram {
         return "Ram{" +
                 "id=" + id +
                 ", capacity=" + capacity +
-                ", Computer_computer_id=" + Computer_computer_id +
+                ", computer_computer_id=" + computer_computer_id +
                 '}';
+    }
+
+    // Builder Pattern
+    public static class RamBuilder {
+        private Integer id;
+        private Integer capacity;
+        private Integer computer_computer_id;
+
+        public RamBuilder() {
+            // No-args constructor
+        }
+
+        public RamBuilder id(Integer id) {
+            this.id = id;
+            return this;
+        }
+
+        public RamBuilder capacity(Integer capacity) {
+            this.capacity = capacity;
+            return this;
+        }
+
+        public RamBuilder computerId(Integer computerId) {
+            this.computer_computer_id = computerId;
+            return this;
+        }
+
+        public Ram build() {
+            return new Ram(this);
+        }
+    }
+
+    private Ram(RamBuilder builder) {
+        this.id = builder.id;
+        this.capacity = builder.capacity;
+        this.computer_computer_id = builder.computer_computer_id;
+    }
+
+    // Listener Pattern
+    public interface RamListener {
+        void onRamCreated(Ram ram);
     }
 }
